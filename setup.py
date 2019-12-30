0fc7a17aa11d9bd9d1ba3a35bcb5965c84b34850
jianlins <jianlinshi.cn@gmail.com>
2019-12-30 05:25:50 -0700

    test appveyor

--- setup.py
+++ setup.py
@@ -26,6 +26,21 @@ extensions = [
     #     include_dirs=include_dirs),
 ]
 
+
+
+def get_version():
+    """Load the version from version.py, without importing it.
+
+    This function assumes that the last line in the file contains a variable defining the
+    version string with single quotes.
+
+    """
+    try:
+        with open('PyFastNER/version.py', 'r') as f:
+            return f.read().split('\n')[0].split('=')[-1].replace('\'', '').strip()
+    except IOError:
+        return "0.0.0a1"
+
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
@@ -35,20 +50,20 @@ setup(
     packages=['PyFastNER'],  # this must be the same as the name above
     package_dir={'PyFastNER': 'PyFastNER'},
     cmdclass={'build_ext': build_ext},
-    version='1.0.8.dev3',
+    version=get_version(),
     description='A fast implementation of dictionary based named entity recognition.',
     author='Jianlin',
     author_email='jianlinshi.cn@gmail.com',
     url='https://github.com/jianlins/PyFastNER',  # use the URL to the github repo
-    download_url='https://github.com/jianlins/PyFastNER/archive/1.0.8.dev2.zip',
     keywords=['PyFastNER', 'ner', 'regex'],
+    license='Apache License',
     long_description=long_description,
     ext_modules=cythonize(extensions, language_level=2),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Development Status :: 3 - Alpha',
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
